3909661011c39e1f3d0f4eb0c526dc44c6b5f67c
DMarzzucco <123565631+DMarzzucco@users.noreply.github.com>
2023-09-26 13:53:20 -0300

    Update proceso-formulario.php

--- proceso-formulario.php
+++ proceso-formulario.php
@@ -1,19 +1,34 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["nombre"];
     $telefono = $_POST["telefono"];
     $email = $_POST["email"];
     $contexto = $_POST["contexto"];
     $mensaje = $_POST["mensaje"];
 
-    $destinatario = "drrkermazyxokv@gmail.com"; 
+// Forma en cómo llegará
+$contenido_mensaje = 'Este mensaje fue enviado por ' . $nombre . ".\r\n";
+$contenido_mensaje .= 'Su teléfono es: ' . $telefono . ".\r\n";
+$contenido_mensaje .= 'Su email es: ' . $email . ".\r\n";
+$contenido_mensaje .= 'Su contexto es: ' . $contexto . ".\r\n";
+$contenido_mensaje .= 'Su mensaje es: ' . $mensaje . ".\r\n";
+$contenido_mensaje .= 'Enviado el: ' . date('d/m/Y', time());
 
-    $asunto = "Nuevo mensaje del formulario";
-    $contenido = "Nombre: $nombre\nTeléfono: $telefono\nEmail: $email\nContexto: $contexto\nMensaje:\n$mensaje";
+// Destinatario y asunto
+$destinatario = "drrkermazyxokv@gmail.com";
+$asunto = "Nuevo mensaje del formulario";
 
-    mail($destinatario, $asunto, $contenido);
+// Cabeceras del correo
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "Content-Type: text/plain; charset=utf-8\r\n";
 
-    // Redirigir al usuario a una página de confirmación o mostrar un mensaje de éxito
-    header("Location: confirmacion.html");
+// Enviar el correo
+if (mail($destinatario, $asunto, utf8_decode($contenido_mensaje), $headers)) {
+    //
+    header("Location: confirmacion_formulario.html");
+} else {
+    // Si el envío de correo falla
+    echo "Error al enviar el correo. Por favor, inténtalo nuevamente.";
 }
+
 ?>
